21e537527c664641afe0c72abf5d5fe73cb73c57
abigcatc683 <abigcatc683@SDS-L-763HC63>
2022-05-27 14:42:11 -0400

    edit code

--- Final-IslandSurvivalPt2/Form1.Designer.cs
+++ Final-IslandSurvivalPt2/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -37,12 +39,38 @@
             this.gameTimer.Interval = 20;
             this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.titleLabel.Location = new System.Drawing.Point(12, 92);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(376, 97);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "title label";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // subtitleLabel
+            // 
+            this.subtitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subtitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.subtitleLabel.Location = new System.Drawing.Point(12, 225);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(376, 108);
+            this.subtitleLabel.TabIndex = 1;
+            this.subtitleLabel.Text = "subtitle label";
+            this.subtitleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Orchid;
-            this.ClientSize = new System.Drawing.Size(434, 511);
+            this.ClientSize = new System.Drawing.Size(400, 500);
+            this.Controls.Add(this.subtitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Island Survival Pt. 2";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -55,6 +83,8 @@
         #endregion
 
         private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subtitleLabel;
     }
 }
 
--- Final-IslandSurvivalPt2/Form1.cs
+++ Final-IslandSurvivalPt2/Form1.cs
@@ -12,29 +12,171 @@ namespace Final_IslandSurvivalPt2
 {
     public partial class Form1 : Form
     {
+        Rectangle hero = new Rectangle(10, 10, 15, 25);
+        int heroSpeed = 10;
+
+        List<Rectangle> enemy = new List<Rectangle>();
+        List<int> enemySpeeds = new List<int>();
+        int enemySize = 10;
+
+        bool leftDown = false;
+        bool rightDown = false;
+        bool upDown = false;
+        bool downDown = false;
+
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush goldBrush = new SolidBrush(Color.Gold);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        Random randGen = new Random();
+        int randValue = 0;
+
+        int counter = 0;
+        string gameState = "waiting";
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        public void GameInitialize()
+        {
+            titleLabel.Text = "";
+            subtitleLabel.Text = "";
+
+            gameTimer.Enabled = true;
+            gameState = "running";
+            enemy.Clear();
+            enemySpeeds.Clear();
+
+        }
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+                case Keys.Up:
+                    upDown = true;
+                    break;
+                case Keys.Down:
+                    downDown = true;
+                    break;
+                case Keys.Space:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        GameInitialize();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        Application.Exit();
+                    }
+                    break;
+            }
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+                case Keys.Up:
+                    upDown = false;
+                    break;
+                case Keys.Down:
+                    downDown = false;
+                    break;
+            }
         }
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
+            //move hero 
+            if (leftDown == true && hero.X > 0)
+            {
+                hero.X -= heroSpeed;
+            }
 
+            if (rightDown == true && hero.X < this.Width - hero.Width)
+            {
+                hero.X += heroSpeed;
+            }
+
+            if (upDown == true && hero.Y > 0)
+            {
+                hero.Y -= heroSpeed;
+            }
+
+            if (downDown == true && hero.Y < this.Height - hero.Height)
+            {
+                hero.Y += heroSpeed;
+            }
+
+            //move enemy 
+            for (int i = 0; i < enemy.Count(); i++)
+            {
+                //find the new postion of y based on speed 
+                int y = enemy[i].Y + enemySpeeds[i];
+
+                //replace the rectangle in the list with updated one using new y 
+                enemy[i] = new Rectangle(enemy[i].X, y, enemySize, enemySize);
+            }
+
+            //add new
+            counter++;
+            if (enemy.Count <= 1)
+            {
+                enemy.Add(new Rectangle(200, 50, 10, 15));
+                enemySpeeds.Add(randGen.Next(1, 4));
+                counter = 0;
+            }
+
+            //removes when off screen
+            //
+            //edit code so enemy goes back to base and then gets removed
+            //currently removes when bottom of screen is reached
+            for (int i = 0; i < enemy.Count(); i++)
+            {
+                if (enemy[i].Y > this.Height - enemySize)
+                {
+                    enemy.RemoveAt(i);
+                    enemySpeeds.RemoveAt(i);
+                }
+            }
+
+            Refresh();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            if (gameState == "waiting")
+            {
+                titleLabel.Text = "IslandSurvival";
+                subtitleLabel.Text = "Press Space Bar to Start or Escape to Exit";
+            }
+            else if (gameState == "running")
+            {
+                //draw hero
+                e.Graphics.FillRectangle(whiteBrush, hero);
 
+                //draw enemies
+                for (int i = 0; i < enemy.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(goldBrush, enemy[i]);
+                }
+            }
         }
     }
 }
